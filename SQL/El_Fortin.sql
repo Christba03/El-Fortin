3be21c65b586b6afcf202a82f5bbf835813c71ce
Christba03 <85861966+Christba03@users.noreply.github.com>
2024-06-04 20:35:48 -0600

    Update El_Fortin.sql
    Insert into de las tablas faltantes

--- SQL/El_Fortin.sql
+++ SQL/El_Fortin.sql
@@ -1,5 +1,5 @@
 CREATE TABLE CLIENTES (
-	Cliente_id SERIAL NOT NULL, 
+	cliente_id SERIAL NOT NULL, 
 	nombre varchar(35) NOT NULL, 
 	apellidop varchar(35) NOT NULL, 
 	apellidom varchar(35) NOT NULL, 
@@ -7,7 +7,7 @@ CREATE TABLE CLIENTES (
 	password varchar(15) NOT NULL, 
 	usuario varchar(20) NOT NULL UNIQUE, 
 	PRIMARY KEY (Cliente_id));
-INSERT INTO CLIENTES(Cliente_id, nombre, apellidop, apellidom, email, password, usuario) 
+INSERT INTO CLIENTES(nombre, apellidop, apellidom, email, password, usuario) 
 	VALUES (?, ?, ?, ?, ?, ?, ?);
 
 CREATE TABLE EMPLEADOS (
@@ -21,11 +21,11 @@ CREATE TABLE EMPLEADOS (
 	telefono char(10) NOT NULL,
 	PRIMARY KEY (empleado_id));
 
-INSERT INTO EMPLEADOS(empleado_id, nombre, apellidop, apellidom, salario, fecha_pago, email, password, usuario, telefono) 
+INSERT INTO EMPLEADOS(nombre, apellidop, apellidom, salario, fecha_pago, email, password, usuario, telefono) 
 	VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
 
 
-CREATE TABLE PEDIDO (
+CREATE TABLE PEDIDOS (
 	pedido_id SERIAL NOT NULL,
 	mesa int NOT NULL,
 	forma_de_pago varchar(12) NOT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE PEDIDO (
 	FOREIGN KEY(Empleado) REFERENCES CLIENTES (Cliente_id),
 	FOREIGN KEY(Cliente) REFERENCES EMPLEADOS (empleado_id));
 
-INSERT INTO PEDIDO(ticket_id, mesa, forma_de_pago, IVA_pagar, costo_total, fecha_venta, descuento, EMPLEADOSEmpleado_id, CLIENTESCliente_id) 
+INSERT INTO PEDIDOS(mesa, forma_de_pago, IVA_pagar, costo_total, fecha_venta, descuento, EMPLEADOSEmpleado_id, CLIENTESCliente_id) 
 	VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
 
 CREATE TABLE PRODUCTOS (
@@ -50,6 +50,8 @@ CREATE TABLE PRODUCTOS (
 	categoria varchar(11) NOT NULL,
 	tipo varchar(25) NOT NULL, stock int4 NOT NULL,
 	PRIMARY KEY (producto_id));
+INSERT INTO PRODUCTOS(nombre, costo, descripcion, categoria, tipo) 
+	VALUES (?, ?, ?, ?, ?);
 
 CREATE TABLE RECETAS (
 	recetas_id SERIAL NOT NULL,
@@ -61,6 +63,10 @@ CREATE TABLE RECETAS (
 	PRIMARY KEY (recetas_id),
 	FOREIGN KEY(producto) REFERENCES PRODUCTOS (producto_id));
 
+INSERT INTO RECETAS("index", nombre, tiempo_preparacion, Descripcion, producto) 
+	VALUES (?, ?, ?, ?, ?);
+
+
 CREATE TABLE "DETALLES DE VENTA" (
 	detalle_id SERIAL NOT NULL,
 	cantidad int NOT NULL,
@@ -70,4 +76,7 @@ CREATE TABLE "DETALLES DE VENTA" (
 	PRIMARY KEY (detalle_id),
 	FOREIGN KEY(pedido) REFERENCES PEDIDOS (pedido_id))
 	FOREIGN KEY(producto) REFERENCES PRODUCTOS (producto_id));
+
+INSERT INTO PEDIDO(cantidad, costo_parcial, pedido, producto) 
+	VALUES (?, ?, ?, ?);
 
