891f75773a23e81593c3d82aecffe25f1dfd59ba
xSE7EN06 <agao6105@gmail.com>
2024-07-09 09:48:00 -0600

    PROYECTO GIOOO
    Todos los archivos

---
+++ PROYECTO INTEGRADOR/js/script.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Selecciona el enlace para abrir la ventana modal
+    var openModalLink = document.querySelector(".open-modal");
+    
+    // Selecciona la ventana modal
+    var modal = document.getElementById("myModal");
+
+    // Selecciona el elemento de cierre de la ventana modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // Agrega un evento de clic al enlace para abrir la ventana modal
+    openModalLink.addEventListener("click", function(event) {
+        event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+        modal.style.display = "block"; // Muestra la ventana modal
+    });
+
+    // Cuando el usuario hace clic en <span> (x), cierra la ventana modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // Cuando el usuario hace clic en cualquier parte fuera de la ventana modal, ciérrala
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
