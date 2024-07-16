e5831b50a4cbeedca793a153a7b269c70ef2536c
xSE7EN06 <agao6105@gmail.com>
2024-07-16 15:10:50 -0600

    El Fortin.sql
    SQL de la base de datos.

--- SQL/El_Fortin.sql
+++ SQL/El_Fortin.sql
@@ -1,8 +1,4 @@
-CREATE TABLE CATEGORIA (
-  categoria_id SERIAL NOT NULL, 
-  nombre_categoria varchar(25) NOT NULL, 
-  PRIMARY KEY (categoria_id));
-  
+
 CREATE TABLE USUARIOS (
   usuario_id SERIAL NOT NULL, 
   user_name varchar(15) NOT NULL UNIQUE, 
@@ -108,6 +104,7 @@ CREATE TABLE PRODUCTOS (
 	precio float NOT NULL,
 	descripcion varchar(125) NOT NULL,
 	stock int NOT NULL,
+	categoria VARCHAR (25) NOT NULL,
 	PRIMARY KEY (producto_id));
 
 CREATE TABLE TIPOS (
@@ -287,5 +284,106 @@ VALUES ( NULL, 'Pendiente', 2);
 
 CREATE VIEW VISTA_VENTAS AS 
 
+/*----------------------------------------INSERT INTO---------------------------------------------*//
 
-INSERT INTO CATEGORIA (nombre_categoria)
+/*TABLA USUARIOS*/
+INSERT INTO USUARIOS (user_name, contrasena, email) VALUES ('jdoe', 'password123', 'jdoe@example.com');
+INSERT INTO USUARIOS (user_name, contrasena, email) VALUES ('asmith', 'securepass', 'asmith@example.com');
+INSERT INTO USUARIOS (user_name, contrasena, email) VALUES ('mjackson', 'moonwalk1', 'mjackson@example.com');
+INSERT INTO USUARIOS (user_name, contrasena, email) VALUES ('lking', 'lion123', 'lking@example.com');
+INSERT INTO USUARIOS (user_name, contrasena, email) VALUES ('kbryant', 'mamba24', 'kbryant@example.com');
+
+/*TABLA PERSONAS*/
+INSERT INTO PERSONAS (Nombre, ApPaterno, ApMaterno, Correo, Telefono) VALUES ('John', 'Doe', 'Smith', 'john.doe@example.com', '1234567890');
+INSERT INTO PERSONAS (Nombre, ApPaterno, ApMaterno, Correo, Telefono) VALUES ('Anna', 'Smith', 'Johnson', 'anna.smith@example.com', '0987654321');
+INSERT INTO PERSONAS (Nombre, ApPaterno, ApMaterno, Correo, Telefono) VALUES ('Michael', 'Jackson', 'Brown', 'michael.jackson@example.com', '1122334455');
+INSERT INTO PERSONAS (Nombre, ApPaterno, ApMaterno, Correo, Telefono) VALUES ('Linda', 'King', 'White', 'linda.king@example.com', '2233445566');
+INSERT INTO PERSONAS (Nombre, ApPaterno, ApMaterno, Correo, Telefono) VALUES ('Kobe', 'Bryant', 'Black', 'kobe.bryant@example.com', '3344556677');
+
+/*TABLA CLIENTES*/
+INSERT INTO CLIENTES (persona_id, usuario_id) VALUES (1, 1);
+INSERT INTO CLIENTES (persona_id, usuario_id) VALUES (2, 2);
+INSERT INTO CLIENTES (persona_id, usuario_id) VALUES (3, 3);
+INSERT INTO CLIENTES (persona_id, usuario_id) VALUES (4, 4);
+INSERT INTO CLIENTES (persona_id, usuario_id) VALUES (5, 5);
+
+/*TABLA EMPLEADOS*/
+INSERT INTO EMPLEADOS (salario, fecha_pago, telefono, usuario_id) VALUES (1600, '2024-07-01', '1234567890', 1);
+INSERT INTO EMPLEADOS (salario, fecha_pago, telefono, usuario_id) VALUES (1800, '2024-07-15', '0987654321', 2);
+INSERT INTO EMPLEADOS (salario, fecha_pago, telefono, usuario_id) VALUES (1600, '2024-07-10', '1122334455', 3);
+INSERT INTO EMPLEADOS (salario, fecha_pago, telefono, usuario_id) VALUES (1500, '2024-07-20', '2233445566', 4);
+INSERT INTO EMPLEADOS (salario, fecha_pago, telefono, usuario_id) VALUES (1900, '2024-07-25', '3344556677', 5);
+
+/*TABLA RECETAS*/
+INSERT INTO RECETAS (nombre, tiempo_preparacion, Descripcion, empleado_id) VALUES ('Pasta Carbonara', '00:30:00', 'Delicious Italian pasta', 1);
+INSERT INTO RECETAS (nombre, tiempo_preparacion, Descripcion, empleado_id) VALUES ('Chicken Curry', '01:00:00', 'Spicy Indian chicken curry', 2);
+INSERT INTO RECETAS (nombre, tiempo_preparacion, Descripcion, empleado_id) VALUES ('Beef Stroganoff', '00:45:00', 'Creamy Russian beef dish', 3);
+INSERT INTO RECETAS (nombre, tiempo_preparacion, Descripcion, empleado_id) VALUES ('Caesar Salad', '00:20:00', 'Fresh Caesar salad with homemade dressing', 4);
+INSERT INTO RECETAS (nombre, tiempo_preparacion, Descripcion, empleado_id) VALUES ('Chocolate Cake', '01:30:00', 'Rich chocolate dessert', 5);
+
+/*TABLA PEDIDOS*/
+INSERT INTO PEDIDOS (mesa, estado, Cliente_id) VALUES (1, 'Pendiente', 1);
+INSERT INTO PEDIDOS (mesa, estado, Cliente_id) VALUES (2, 'En Proceso', 2);
+INSERT INTO PEDIDOS (mesa, estado, Cliente_id) VALUES (3, 'Completado', 3);
+INSERT INTO PEDIDOS (mesa, estado, Cliente_id) VALUES (4, 'Cancelado', 4);
+INSERT INTO PEDIDOS (mesa, estado, Cliente_id) VALUES (5, 'Pendiente', 5);
+
+/*TABLA DETALLE_PEDIDO*/
+INSERT INTO DETALLES_PEDIDOS (cantidad, fecha_pedido, producto_id, pedido_id) VALUES (2, '2024-07-01', 1, 1);
+INSERT INTO DETALLES_PEDIDOS (cantidad, fecha_pedido, producto_id, pedido_id) VALUES (1, '2024-07-02', 2, 2);
+INSERT INTO DETALLES_PEDIDOS (cantidad, fecha_pedido, producto_id, pedido_id) VALUES (3, '2024-07-03', 3, 3);
+INSERT INTO DETALLES_PEDIDOS (cantidad, fecha_pedido, producto_id, pedido_id) VALUES (4, '2024-07-04', 4, 4);
+INSERT INTO DETALLES_PEDIDOS (cantidad, fecha_pedido, producto_id, pedido_id) VALUES (5, '2024-07-05', 5, 5);
+
+/*TABLA FORMAS_PAGOS_VENTAS*/
+INSERT INTO FORMAS_PAGOS_VENTAS (forma_pago_id, venta_id) VALUES (1, 1);
+INSERT INTO FORMAS_PAGOS_VENTAS (forma_pago_id, venta_id) VALUES (2, 2);
+INSERT INTO FORMAS_PAGOS_VENTAS (forma_pago_id, venta_id) VALUES (3, 3);
+INSERT INTO FORMAS_PAGOS_VENTAS (forma_pago_id, venta_id) VALUES (4, 4);
+INSERT INTO FORMAS_PAGOS_VENTAS (forma_pago_id, venta_id) VALUES (5, 5);
+
+/*TABLA FORMAS_PAGOS*/
+INSERT INTO FORMAS_PAGOS (nombre) VALUES ('Tarjeta de Crédito');
+INSERT INTO FORMAS_PAGOS (nombre) VALUES ('Tarjeta de Débito');
+INSERT INTO FORMAS_PAGOS (nombre) VALUES ('Efectivo');
+INSERT INTO FORMAS_PAGOS (nombre) VALUES ('Transferencia Bancaria');
+INSERT INTO FORMAS_PAGOS (nombre) VALUES ('PayPal');
+
+/*TABLAS VENTAS*/
+INSERT INTO VENTAS (IVA_pagar, pago_total, fecha_venta, descuento_venta, empleado_id, Cliente_id) VALUES (16.0, 100.0, '2024-07-01 10:00:00', 5, 1, 1);
+INSERT INTO VENTAS (IVA_pagar, pago_total, fecha_venta, descuento_venta, empleado_id, Cliente_id) VALUES (16.0, 200.0, '2024-07-02 11:00:00', 10, 2, 2);
+INSERT INTO VENTAS (IVA_pagar, pago_total, fecha_venta, descuento_venta, empleado_id, Cliente_id) VALUES (16.0, 150.0, '2024-07-03 12:00:00', 15, 3, 3);
+INSERT INTO VENTAS (IVA_pagar, pago_total, fecha_venta, descuento_venta, empleado_id, Cliente_id) VALUES (16.0, 300.0, '2024-07-04 13:00:00', 20, 4, 4);
+INSERT INTO VENTAS (IVA_pagar, pago_total, fecha_venta, descuento_venta, empleado_id, Cliente_id) VALUES (16.0, 250.0, '2024-07-05 14:00:00', 25, 5, 5);
+
+/*TABLA_DETALLES_VENTA*/
+INSERT INTO DETALLES_VENTA (venta_id, subtotal, descuento_articulo, p_cantidad) VALUES (1, 100.0, 5, 2);
+INSERT INTO DETALLES_VENTA (venta_id, subtotal, descuento_articulo, p_cantidad) VALUES (2, 200.0, 10, 3);
+INSERT INTO DETALLES_VENTA (venta_id, subtotal, descuento_articulo, p_cantidad) VALUES (3, 150.0, 15, 1);
+INSERT INTO DETALLES_VENTA (venta_id, subtotal, descuento_articulo, p_cantidad) VALUES (4, 300.0, 20, 4);
+INSERT INTO DETALLES_VENTA (venta_id, subtotal, descuento_articulo, p_cantidad) VALUES (5, 250.0, 25, 5);
+
+/*TABLA PRODUCTOS*/
+INSERT INTO PRODUCTOS (nombre, precio, descripcion, stock, categoria) VALUES ('Producto A', 10.0, 'Descripción A', 100, 'Categoría 1');
+INSERT INTO PRODUCTOS (nombre, precio, descripcion, stock, categoria) VALUES ('Producto B', 20.0, 'Descripción B', 200, 'Categoría 2');
+INSERT INTO PRODUCTOS (nombre, precio, descripcion, stock, categoria) VALUES ('Producto C', 30.0, 'Descripción C', 300, 'Categoría 3');
+INSERT INTO PRODUCTOS (nombre, precio, descripcion, stock, categoria) VALUES ('Producto D', 40.0, 'Descripción D', 400, 'Categoría 4');
+INSERT INTO PRODUCTOS (nombre, precio, descripcion, stock, categoria) VALUES ('Producto E', 50.0, 'Descripción E', 500, 'Categoría 5');
+
+/*TABLA TIPOS*/
+INSERT INTO TIPOS (producto_id, nombre) VALUES (1, 'Tipo 1');
+INSERT INTO TIPOS (producto_id, nombre) VALUES (2, 'Tipo 2');
+INSERT INTO TIPOS (producto_id, nombre) VALUES (3, 'Tipo 3');
+INSERT INTO TIPOS (producto_id, nombre) VALUES (4, 'Tipo 4');
+INSERT INTO TIPOS (producto_id, nombre) VALUES (5, 'Tipo 5');
+
+/*TABLA BITACORA_RECETAS*/
+INSERT INTO BITACORA_RECETAS (table_name, table_id, description, operacion) VALUES ('RECETAS', '1', 'Descripción de cambio 1', 'INSERT');
+INSERT INTO BITACORA_RECETAS (table_name, table_id, description, operacion) VALUES ('RECETAS', '2', 'Descripción de cambio 2', 'UPDATE');
+INSERT INTO BITACORA_RECETAS (table_name, table_id, description, operacion) VALUES ('RECETAS', '3', 'Descripción de cambio 3', 'DELETE');
+INSERT INTO BITACORA_RECETAS (table_name, table_id, description, operacion) VALUES ('RECETAS', '4', 'Descripción de cambio 4', 'INSERT');
+INSERT INTO BITACORA_RECETAS (table_name, table_id, description, operacion) VALUES ('RECETAS', '5', 'Descripción de cambio 5', 'UPDATE');
+
+
+
+
