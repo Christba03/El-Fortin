cdf5cc568006f7269073e03efa0f0001b79f66e1
xSE7EN06 <agao6105@gmail.com>
2024-07-21 00:11:29 -0600

    Cambios en la pagina web
    Cambios en toda la pagina e implementacion de js

--- PROYECTO INTEGRADOR/js/script.js
+++ PROYECTO INTEGRADOR/js/script.js
@@ -40,3 +40,110 @@ function searchTable() {
     }
   });
 }
+
+$(document).ready(function(){
+
+  //primero vamos a crear un arreglo con los datos de los usuarios que se van a mostrar de ejemplo
+  let arreglo = [
+    {
+      id: 1,
+      nombre: "Jose",
+      apellidoPaterno: "Perez",
+      apellidoMaterno: "Gomez",
+      correo: "jose@gmail.com",
+      contrasena: "jose122",
+    },
+    {
+      id: 2,
+      nombre: "Angel",
+      apellidoPaterno: "Rivera",
+      apellidoMaterno: "Sanchez",
+      correo: "rivera@gmail.com",
+      contrasena: "rivera23",
+    },
+  ];
+
+  //se creara una funcion para cargar los usuarios que esten en la tabla.
+  function loadEmployes() {
+    let empleados = arreglo;
+    let empleadoTableBody = $("#employe-table-body");
+    empleadoTableBody.empty();
+    empleados.forEach((empleado) => {
+      empleadoTableBody.append(`
+                    <tr>
+                        <td>${empleado.id}</td>
+                        <td>${empleado.nombre}</td>
+                        <td>${empleado.apellidoPaterno}</td>
+                        <td>${empleado.apellidoMaterno}</td>
+                        <td>${empleado.correo}</td>
+                        <td>${empleado.contrasena}</td>
+                        <td>
+                           <a href="" class="link-primary"><i class="fa-solid fa-pen-to-square fs-5 me-3"></i></a>
+                           <a href="" class="link-danger"><i class="fa-solid fa-trash fs-5 me-3"></i></a>
+                           <a href="" class="link-success"><i class="fa-solid fa-eye fs-5"></i></a>
+                        </td>
+                    </tr>
+                `);
+    });
+  }
+
+  function alert(){
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: "Your work has been saved",
+      showConfirmButton: false,
+      timer: 1500
+    });
+  }
+
+
+  $("#formEmpleados").submit(function (event){
+    event.preventDefault();
+    let userId = $("#empleado-id").val();
+    let empleadoName = $("#nombreEmpleado").val().split(" ");
+    let apellidoPaterno = $("#apPaterno").val();
+    let apellidoMaterno = $("#apMaterno").val();
+    let correo = $("#correo").val();
+    let contrasena = $("#contrasena").val();
+    let method = userId ? "PUT" : "POST";
+
+    if(method == "POST"){
+      let newId = arreglo[arreglo.length - 1].id + 1;
+      arreglo.push({
+        id: newId,
+        nombre: empleadoName,
+        apellidoPaterno: apellidoPaterno,
+        apellidoMaterno: apellidoMaterno,
+        correo: correo,
+        contrasena: contrasena,
+      });
+    }else {
+      let objeto = searchObject(userId);
+
+      objeto.nombre = empleadoName;
+      objeto.apellidoPaterno= apellidoPaterno;
+      objeto.apellidoMaterno= apellidoMaterno;
+      objeto.correo = correo;
+      objeto.contrasena= contrasena;
+    }
+    loadEmployes();
+    alert();
+    $("#modalUsuarios").modal("hide");
+  });
+
+  function searchObject(id) {
+    let objeto = {};
+    for (let i = 0; i < arreglo.length; i++) {
+      if (id == arreglo[i].id) {
+        objeto = arreglo[i];
+        break;
+      }
+    }
+    return objeto;
+  }
+
+   // Inicializar la tabla de usuarios
+
+   loadEmployes();
+});
