2347a6a304652dfc0c9f9508046323d5d01cfffd
Angelito18z <20233L001142@utcv.edu.mx>
2024-08-04 16:59:55 -0600

    Cambios en modal y formato
    los modales de operaciones de empleados los pase a español y le di formato a una libreria

--- PROYECTO INTEGRADOR/js/script.js
+++ PROYECTO INTEGRADOR/js/script.js
@@ -172,26 +172,26 @@ $(document).ready(function(){
   
       if (indice !== -1) {
         Swal.fire({
-          title: "Are you sure?",
-          text: "You won't be able to revert this!",
+          title: "¿Estas seguro?",
+          text: "¡No podrás revertir esto!",
           icon: "warning",
           showCancelButton: true,
           confirmButtonColor: "#09A62E",
           cancelButtonColor: "#d33",
-          confirmButtonText: "Yes, delete it!"
+          confirmButtonText: "Si, bórralo!"
         }).then((result) => {
           if (result.isConfirmed) {
             arreglo.splice(indice, 1);
             loadEmployes();
             Swal.fire({
-              title: "Deleted!",
-              text: "Your file has been deleted.",
+              title: "Borrado!",
+              text: "Su registro ha sido eliminado.",
               icon: "success"
             });
           }else if(result.dismiss === Swal.DismissReason.cancel){
             Swal.fire({
-              title: "Cancelled",
-              text: "Your imaginary file is safe :)",
+              title: "Cancelado",
+              text: "Tu registro se salvó :)",
               icon: "error"
             })
           }
