91710c6c3ee8a2fc3d77ffeba6acabd775b56fb8
Christba03 <christba455@gmail.com>
2024-08-04 17:01:43 -0600

    cambios

---
+++ PROYECTO INTEGRADOR/js/apis/api.empleados.js
@@ -0,0 +1,213 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Obtener todos los enlaces de navegación
+  const navLinks = document.querySelectorAll('#nav-links .nav-link');
+
+  // Obtener la URL actual y extraer el nombre del archivo
+  const currentUrl = window.location.pathname.split('/').pop();
+
+  // Iterar sobre los enlaces y añadir la clase 'active' al enlace que coincide con la URL actual
+  navLinks.forEach(link => {
+    if (link.getAttribute('href') === currentUrl) {
+      link.classList.add('active');
+    }
+  });
+});
+
+
+function searchTable() {
+  // Obtener el valor del input de búsqueda
+  let input = document.getElementById("buscar").value.toLowerCase();
+  // Obtener todas las filas de la tabla
+  let rows = document.querySelectorAll("#contenidoTabla table tbody tr");
+
+  // Iterar sobre cada fila y mostrar/ocultar según el criterio de búsqueda
+  rows.forEach(row => {
+    let match = false;
+    // Obtener las celdas de la fila actual
+    let cells = row.getElementsByTagName("td");
+    // Iterar sobre las celdas y verificar si alguna coincide con la búsqueda
+    Array.from(cells).forEach(cell => {
+      let cellText = cell.textContent.toLowerCase();
+      if (cellText.includes(input)) {
+        match = true;
+      }
+    });
+    // Mostrar u ocultar la fila según el resultado de la búsqueda
+    if (match) {
+      row.style.display = "";
+    } else {
+      row.style.display = "none";
+    }
+  });
+}
+
+$(document).ready(function(){
+
+  //primero vamos a crear un arreglo con los datos de los usuarios que se van a mostrar de ejemplo
+  let arreglo = [
+    {
+      id: 1,
+      nombre: "Jose",
+      apellidoPaterno: "Perez",
+      apellidoMaterno: "Gomez",
+      correo: "jose@gmail.com",
+      contrasena: "jose122",
+    },
+    {
+      id: 2,
+      nombre: "Angel",
+      apellidoPaterno: "Rivera",
+      apellidoMaterno: "Sanchez",
+      correo: "rivera@gmail.com",
+      contrasena: "rivera23",
+    },
+  ];
+
+  //se creara una funcion para cargar los usuarios que esten en la tabla.
+  function loadEmployes() {
+    let empleados = arreglo;
+    let empleadoTableBody = $("#employe-table-body");
+    empleadoTableBody.empty();
+    empleados.forEach((empleado) => {
+      empleadoTableBody.append(`
+                    <tr>
+                        <td>${empleado.id}</td>
+                        <td>${empleado.nombre}</td>
+                        <td>${empleado.apellidoPaterno}</td>
+                        <td>${empleado.apellidoMaterno}</td>
+                        <td>${empleado.correo}</td>
+                        <td>${empleado.contrasena}</td>
+                        <td>
+                           <button class="btn btn-sm text-bg-secondary edit-user-btn" data-id="${empleado.id}"><i class="fa-solid fa-pen-to-square fs-6"></i></button>
+                           <button class="btn btn-sm text-bg-primary delete-user-btn" data-id="${empleado.id}"><i class="fa-solid fa-trash fs-6"></i></button>
+                        </td>
+                    </tr>
+                `);
+    });
+  }
+
+  function alert(){
+    Swal.fire({
+      icon: "success",
+      title: "Guardado",
+    });
+  }
+
+
+  //funcion para agregar los usuarios
+  $("#formEmpleados").submit(function (event){
+    event.preventDefault();
+    let userId = $("#empleado-id").val();
+    let empleadoName = $("#nombreEmpleado").val().split(" ");
+    let apellidoPaterno = $("#apPaterno").val();
+    let apellidoMaterno = $("#apMaterno").val();
+    let correo = $("#correo").val();
+    let contrasena = $("#contrasena").val();
+    let method = userId ? "PUT" : "POST";
+
+    if(method == "POST"){
+      let newId = arreglo[arreglo.length - 1].id + 1;
+      arreglo.push({
+        id: newId,
+        nombre: empleadoName,
+        apellidoPaterno: apellidoPaterno,
+        apellidoMaterno: apellidoMaterno,
+        correo: correo,
+        contrasena: contrasena,
+      });
+    }else {
+      let objeto = searchObject(userId);
+
+      objeto.nombre = empleadoName;
+      objeto.apellidoPaterno= apellidoPaterno;
+      objeto.apellidoMaterno= apellidoMaterno;
+      objeto.correo = correo;
+      objeto.contrasena= contrasena;
+    }
+    loadEmployes();
+    alert();
+    $("#modalEmpleados").modal("hide");
+  });
+
+  function searchObject(id) {
+    let objeto = {};
+    for (let i = 0; i < arreglo.length; i++) {
+      if (id == arreglo[i].id) {
+        objeto = arreglo[i];
+        break;
+      }
+    }
+    return objeto;
+  }
+
+
+   // Editar usuario
+   $(document).on("click", ".edit-user-btn", function () {
+    let userId = $(this).data("id");
+    let objeto = searchObject(userId);
+
+    let employe = objeto;
+    $("#empleado-id").val(employe.id);
+    $("#nombreEmpleado").val(employe.nombre);
+    $("#apPaterno").val(employe.apellidoPaterno);
+    $("#apMaterno").val(employe.apellidoPaterno);
+    $("#correo").val(employe.correo);
+    $("#contrasena").val(employe.contrasena);
+    $("#modalEmpleadosLabel").text("Editar Empleado");
+    $("#modalEmpleados").modal("show");
+  });
+
+
+  // Eliminar usuario
+    // Eliminar usuario
+    $(document).on("click", ".delete-user-btn", function () {
+      let empleadoId = $(this).data("id");
+      let indice = -1;
+      for (let i = 0; i < arreglo.length; i++) {
+        if (empleadoId == arreglo[i].id) {
+          indice = i;
+          break;
+        }
+      }
+  
+      if (indice !== -1) {
+        Swal.fire({
+          title: "Are you sure?",
+          text: "You won't be able to revert this!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#09A62E",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+          if (result.isConfirmed) {
+            arreglo.splice(indice, 1);
+            loadEmployes();
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success"
+            });
+          }else if(result.dismiss === Swal.DismissReason.cancel){
+            Swal.fire({
+              title: "Cancelled",
+              text: "Your imaginary file is safe :)",
+              icon: "error"
+            })
+          }
+        });
+      }
+    });
+
+    // Resetear modal al cerrarlo
+    $("#modalEmpleados").on("hidden.bs.modal", function () {
+      $("#formEmpleados")[0].reset();
+      $("#empleado-id").val("");
+      $("#modalEmpleadosLabel").text("Agregar Empleado");
+    });
+  
+
+   // Inicializar la tabla de usuarios
+
+   loadEmployes();
+});
---
+++ PROYECTO INTEGRADOR/js/apis/api.productos.js
@@ -0,0 +1,170 @@
+
+//Esta función se manda a llamar cuando se termina de cargar por completo la página
+$(document).ready(function() {
+    // Esta es la URL de la API donde se encuentran los usuarios.
+    const apiUrl = 'http://test.utcv.edu.mx/api/products';
+
+    // Función para cargar los usuarios desde la API.
+    function loadProducts() {
+        $.ajax({ // es la forma de acceder a las apis mediante Ajax
+            url: apiUrl, // URL de la API para obtener los usuarios.
+            method: 'GET', // Método HTTP para obtener datos.
+            success: function(response) {// la variable “response” trae el resultado del api
+                // La respuesta contiene los datos de los usuarios.
+                let products = response; // data es una propiedad del json de la respuesta
+                // Se selecciona el cuerpo de la tabla donde se mostrarán los usuarios.
+                let productTableBody = $('#product-table-body');
+                productTableBody.empty(); // Se vacía la tabla antes de llenarla.
+
+                // Se recorre cada usuario y se añade una fila en la tabla para cada uno.
+                products.forEach((product) => {
+                    productTableBody.append(`
+                        <tr>
+                            <td>${product.id}</td>
+                            <td>${product.product} </td>
+                            <td>${product.price}</td>
+                            <td>${product.bar_code} </td>
+                            <td>${product.stock}</td>
+                            <td>${product.description}</td>
+                        <td>
+                           <button class="btn btn-sm text-bg-secondary edit-product-btn" data-id="${product.id}"><i class="fa-solid fa-pen-to-square fs-6"></i></button>
+                           <button class="btn btn-sm text-bg-primary delete-product-btn" data-id="${product.id}"><i class="fa-solid fa-trash fs-6"></i></button>
+                        </td>
+                        </tr>
+                    `);
+                });
+            }
+            
+        });
+    }
+    function alert(){
+        Swal.fire({
+          icon: "success",
+          title: "Guardado",
+        });
+      }
+
+
+    // Evento que se dispara al enviar el formulario para agregar o editar un usuario.
+    $('#product-form').submit(function(event) {
+        event.preventDefault(); // Evita que el formulario se envíe de la manera tradicional.
+
+        let productId = $('#product-id').val(); // ID del usuario (si se está editando).
+        let productName = $('#product-name').val().split(' '); // Nombre del usuario.
+        let productEmail = $('#product-email').val(); // Email del usuario.
+
+        // Se determina si se trata de agregar (POST) o editar (PUT) un usuario.
+        let method = productId ? 'PUT' : 'POST';
+        let url = productId ? `${apiUrl}/${productId}` : apiUrl;
+
+        // Se realiza la llamada AJAX para agregar o editar el usuario.
+        $.ajax({
+            url: url, // URL de la API.
+            method: method, // Método HTTP (POST o PUT).
+            contentType: 'application/json', // Tipo de contenido de los datos enviados.
+            data: JSON.stringify({
+                first_name: productName[0], // Primer nombre.
+                last_name: productName[1] || '', // Segundo nombre (si lo hay).
+                email: productEmail // Email.
+            }),
+            success: function() {
+                // Si la operación es exitosa, se recargan los usuarios y se muestra una alerta de éxito.
+                loadProducts();
+                showAlert('Producto guardado exitosamente', 'success');
+                $('#productModal').modal('hide'); // Se cierra el modal.
+            },
+            error: function() {
+                // Si hay un error, se muestra una alerta de error.
+                showAlert('Error al guardar el usuario', 'danger');
+            }
+        });
+    });
+
+    // Evento que se dispara al hacer clic en el botón de editar un usuario.
+    $("#formProductos").on('click', '.edit-product-btn', function() {
+        let productId = $(this).data('id'); // Se obtiene el ID del usuario a editar.
+
+        // Se realiza la llamada AJAX para obtener los datos del usuario.
+        $.ajax({
+            url: `${apiUrl}/${productId}`, // URL de la API con el ID del usuario.
+            method: 'GET', // Método HTTP para obtener datos.
+            success: function(response) {
+                let product = response; // Datos del usuario.
+
+                // Se rellenan los campos del formulario con los datos del usuario.
+                $("#product-id").val(product.id);
+                $("#product").val(product.product);
+                $("#price").val(product.price);
+                $("#bar_code").val(product.bar_code);
+                $("#stock").val(product.stock);
+                $("#description").val(product.description);
+                $("#modalEmpleadosLabel").text("Editar Empleado");
+                $("#modalEmpleados").modal("show");                $('#product-name').val(`${product.first_name} ${product.last_name}`);
+                $('#product-email').val(product.email);
+                $('#productModalLabel').text('Editar Usuario'); // Se cambia el título del modal.
+                $('#productModal').modal('show'); // Se muestra el modal.
+            }
+        });
+        loadEmployes();
+        alert();
+        $("#modalEmpleados").modal("hide");
+    });
+
+    // Evento que se dispara al hacer clic en el botón de eliminar un usuario.
+    $(document).on('click', '.delete-product-btn', function() {
+        let productId = $(this).data('id'); // Se obtiene el ID del usuario a eliminar.
+
+        // Se realiza la llamada AJAX para eliminar el usuario.
+        $.ajax({
+            url: `${apiUrl}/${productId}`, // URL de la API con el ID del usuario.
+            method: 'DELETE', // Método HTTP para eliminar datos.
+            success: function() {
+                // Si la operación es exitosa, se recargan los usuarios y se muestra una alerta de éxito.
+                loadProducts();
+                showAlert('Usuario eliminado exitosamente', 'success');
+            },
+            error: function() {
+                // Si hay un error, se muestra una alerta de error.
+                showAlert('Error al eliminar el usuario', 'danger');
+            }
+        });
+    });
+
+    // Evento que se dispara al cerrar el modal.
+    $('#productModal').on('hidden.bs.modal', function() {
+        $('#product-form')[0].reset(); // Se resetea el formulario.
+        $('#product-id').val(''); // Se vacía el campo de ID.
+        $('#productModalLabel').text('Agregar Usuario'); // Se cambia el título del modal.
+    });
+
+    // Se inicializan los usuarios al cargar la página.
+    loadProducts();
+});
+
+
+function searchTable() {
+    // Obtener el valor del input de búsqueda
+    let input = document.getElementById("buscar").value.toLowerCase();
+    // Obtener todas las filas de la tabla
+    let rows = document.querySelectorAll("#contenidoTabla table tbody tr");
+  
+    // Iterar sobre cada fila y mostrar/ocultar según el criterio de búsqueda
+    rows.forEach(row => {
+      let match = false;
+      // Obtener las celdas de la fila actual
+      let cells = row.getElementsByTagName("td");
+      // Iterar sobre las celdas y verificar si alguna coincide con la búsqueda
+      Array.from(cells).forEach(cell => {
+        let cellText = cell.textContent.toLowerCase();
+        if (cellText.includes(input)) {
+          match = true;
+        }
+      });
+      // Mostrar u ocultar la fila según el resultado de la búsqueda
+      if (match) {
+        row.style.display = "";
+      } else {
+        row.style.display = "none";
+      }
+    });
+  }
