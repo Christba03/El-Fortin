2355393d31f9f314f6235e2c2bb17b04a3b77811
Angelito18z <20233L001142@utcv.edu.mx>
2024-08-04 23:31:58 -0600

    Cambios en modales de ventas y recetas

--- PROYECTO INTEGRADOR/js/apis/api.ventas.js
+++ PROYECTO INTEGRADOR/js/apis/api.ventas.js
@@ -159,26 +159,26 @@ document.addEventListener('DOMContentLoaded', () => {
     
         if (indice !== -1) {
           Swal.fire({
-            title: "Are you sure?",
-            text: "You won't be able to revert this!",
+            title: "Estas seguro?",
+            text: "No podras revertir el cambio!",
             icon: "warning",
             showCancelButton: true,
             confirmButtonColor: "#09A62E",
             cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, delete it!"
+            confirmButtonText: "Si, bórralo!"
           }).then((result) => {
             if (result.isConfirmed) {
               arreglo.splice(indice, 1);
               loadEmployes();
               Swal.fire({
                 title: "Borrado!",
-                text: "Your file has been deleted.",
+                text: "Tu registro fue eliminado.",
                 icon: "success"
               });
             }else if(result.dismiss === Swal.DismissReason.cancel){
               Swal.fire({
-                title: "Cancelled",
-                text: "Your imaginary file is safe :)",
+                title: "Cancelado",
+                text: "Tu registro fue salvado :)",
                 icon: "error"
               })
             }
